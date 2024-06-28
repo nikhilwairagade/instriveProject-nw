e3fe2f4726079a65580eef6c5995dab4a9601bfd
Nikhil Wairagade <nikhilwairagade@Nikhils-MacBook-Air.local>
2024-06-28 14:28:24 +0530

    sort query added

--- product/controller/company.js
+++ product/controller/company.js
@@ -1,44 +1,65 @@
-const {Company,companyJoiSchema} = require('../model/company');
+const { Company, companyJoiSchema } = require('../model/company');
 const companyRouter = require('express').Router();
 const _ = require('lodash');
 const limit = parseInt(process.env.limit);
 
 
-companyRouter.addCompanyDetails = async (req,res) =>{
+companyRouter.addCompanyDetails = async (req, res) => {
     try {
 
-        const {error} = companyJoiSchema.validate(req.body);
-        if(error) res.status(400).json({message:error.details[0].message});
+        const { error } = companyJoiSchema.validate(req.body);
+        if (error) res.status(400).json({ message: error.details[0].message });
 
 
-        let company = new Company(_.pick(req.body, ["name", "headOfficeAddress", "country","postalCode", "city", "countryCode", "contactNo", "webSiteUrl", "logo","keyContactPerson","additionalInfo","productPortfolio","managementTeamDetails"]))
-        
+        let company = new Company(_.pick(req.body, ["name", "headOfficeAddress", "country", "postalCode", "city", "countryCode", "contactNo", "webSiteUrl", "logo", "keyContactPerson", "additionalInfo", "productPortfolio", "managementTeamDetails"]))
+
         await company.save();
 
-        res.status(200).json({message:"Company details saved successfully"});
-        
+        res.status(200).json({ message: "Company details saved successfully" });
+
     } catch (err) {
-        console.log("Errorn in companyRouter",err);
-        res.status(502).json({message:"Something went wrong"});
+        console.log("Errorn in companyRouter", err);
+        res.status(502).json({ message: "Something went wrong" });
     }
 }
 
 
-companyRouter.getCompanyDetails = async (req,res) => {
+companyRouter.getCompanyDetails = async (req, res) => {
     try {
-        let pageNo = req.query.pageNo || 1;
+        let reqQuery = req.query
+        let pageNo = reqQuery.pageNo || 1;
 
-        const calSkip = (pageNo - 1) * limit; 
+        const calSkip = (pageNo - 1) * limit;
 
-        let companyDetails = await Company.aggregate([
-            {$skip: calSkip},
-            {$limit: limit}
-        ])
+        let pipeline = [];
 
-        return res.status(200).json({message:"Success", data: companyDetails});
+        if (reqQuery.sortKey) {
+            let sortKey = reqQuery.sortKey
+            let sortOrder = reqQuery.sortOrder;
+            let order = sortOrder == 'asc' ? 1 : -1;
+            let obj = {};
+            obj[sortKey] = order;
+            pipeline.push({'$sort':obj});
+        }
+
+        const pagination = [{
+            $skip: calSkip
+        },
+        {
+            $limit: limit
+        }]
+
+        pipeline.push(...pagination);
+
+
+        console.log("----", pipeline);
+
+        let companyDetails = await Company.aggregate(pipeline);
+
+        return res.status(200).json({ message: "Success", data: companyDetails });
     } catch (err) {
-        console.log("Errorn in get company details",err);
-        res.status(502).json({message:"Something went wrong"});
+        console.log("Errorn in get company details", err);
+        res.status(502).json({ message: "Something went wrong" });
     }
 }
 
